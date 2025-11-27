76b8231bc9a4284168c96b645991dca4f4603d1a
Pintu <pintu@Pintus-Laptop.local>
2025-11-27 15:39:55 +0530

    add deployed url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ function App() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/chat", {
+      const res =  await fetch(`${process.env.REACT_APP_BACKEND_URL}/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userMsg.text })
